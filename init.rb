8edab5c818202e6c945bcd9bb83e7f9a535f63bc
noborunrun <yanagisawano@gmail.com>
2009-04-29 20:09:37 +0900

    init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,10 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_birdseye do
   name 'Redmine Birdseye plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Yanagisawa Noboru'
+  description 'You will able to use Birds Eyed View.'
   version '0.0.1'
+
+menu :application_menu, :birdseye, { :controller => 'birdseye_main', :action => 'index' }, :caption => 'BirdsEye', :last => true
+
 end
